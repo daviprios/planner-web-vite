f84daac4ec69d4b725502c6e06314a9ee820f85f
Davi Paim Rios <davipaimrios@gmail.com>
2022-02-22 15:09:30 -0300

    Add responsive Event to Calendar

--- src/app/views/Agenda/Events/index.tsx
+++ src/app/views/Agenda/Events/index.tsx
@@ -1,61 +1,85 @@
 import { useContext, useEffect, useRef, useState } from 'react'
 import styles from './index.module.sass'
+
 import { DatabaseContext } from '$app/provider/DatabaseProvider'
 import DatabaseManager from '$data/database/DatabaseManager'
 import StorageRequest from '$data/StorageRequest'
+
 import ReminderEvent from './ReminderEventForm'
 import { useOnClickOutside } from 'usehooks-ts'
+
+const getFullDayRange = (timestamp: number) => {
+  const date = new Date(timestamp)
+  const [day, month, year, timezone] = [date.getDate(), date.getMonth(), date.getFullYear(), date.getTimezoneOffset()]
+  console.log(timezone)
+  
+  const dateRange = new Date(year, month, day)
+  const range: [number, number] = [dateRange.getTime(), 0]
+  dateRange.setDate(dateRange.getDate() + 1)
+  range[1] = (dateRange.getTime())
+
+  return range
+}
 
 const Events = (props: { date: number }) => {
   const { date } = props
   const { database, reconnect } = useContext(DatabaseContext)
+
   const [events, setEvents] = useState<JSX.Element[]>([])
+  useEffect(() => {
+    reconnect()
+  }, [])
 
   const getEvents = async (): Promise<JSX.Element[]> => {
-    const result = database && await StorageRequest.read<'ReminderEvents'>(database, 'ReminderEvents', { get: 'ALL', return: 'VALUE' })
+    const dayRange = getFullDayRange(date)
+    const result = database && await StorageRequest.read<'ReminderEvents'>(database, 'ReminderEvents', { get: 'ALL', return: 'VALUE', index: {
+      table: 'timeStart',
+      search: IDBKeyRange.bound(dayRange[0], dayRange[1])
+    } })
     if(!result) return [<li>Empty</li>]
     let list: JSX.Element[] = []
     list = result.map((item) => {
       const { key, value } = item
       if(!value) return <></>
 
+      const { timeStart, timeEnd } = value
+      const [dateStart, dateEnd] = [new Date(timeStart), new Date(timeEnd ?? 0)]
+      const [startHour, startMinute, endHour, endMinute] = [dateStart.getHours(), dateStart.getMinutes(), dateEnd.getHours(), dateEnd.getMinutes()]
+
       return (
-        <li key={`l${key}`}>
+        <li key={`l${key}`} className={styles.eventItem}>
           <div>
-            <span>{value.name}</span>
-            <span>{new Date(value.timeStart).toUTCString()}</span>
-            {value.timeEnd ? <span> - {new Date(value.timeEnd).toUTCString()}</span> : <></>}
+            <h3>{value.name}</h3>
+            <p>
+              <span>{startHour}:{startMinute < 10 ? '0' : ''}{startMinute}</span> {value.timeEnd ? <span> - {endHour}:{endMinute < 10 ? '0' : ''}{endMinute}</span> : <></>}
+            </p>
           </div>
           <div>
-            <span>{value.description}</span>
+            <p>{value.description}</p>
+            {value.tags?.map((tag) => <span key={tag}>{tag}</span>)}
           </div>
         </li>
       )
     })
     return list
   }
-
   useEffect(() => {
-    reconnect()
     getEvents().then(res => setEvents(res))
-  }, [])
+  }, [database, date])
 
   const [showForm, setShowForm] = useState(false)
-
   const formRef = useRef<HTMLFormElement>(null)
   useOnClickOutside(formRef, () => setShowForm(false))
 
   return (
     <section className={styles.eventsContainer}>
-      <h2>Events</h2>
-      <button onClick={() => DatabaseManager.deleteDatabase('database')}>delete DATABASE</button>
-      <button onClick={() => setShowForm(!showForm)}>Add Event</button>
-      <button onClick={() => getEvents().then(res => setEvents(res))}>Update</button>
-      <ul>
-        {
-          //database && StorageRequest.create<'ReminderEvents'>(database, 'ReminderEvents',
-          //{ name: 'Novo Evento', createdAt: Date.now(), fullDay: false, timeStart: Date.now(), updatedAt: Date.now() })
-        }
+      <h2 className={styles.title}>Events</h2>
+      <div className={styles.buttonContainer}>
+        <button onClick={() => DatabaseManager.deleteDatabase('database')}>delete DATABASE</button>
+        <button onClick={() => setShowForm(!showForm)}>Add Event</button>
+        <button onClick={() => getEvents().then(res => setEvents(res))}>Update</button>
+      </div>
+      <ul className={styles.eventList}>
         {events}
       </ul>
       <section style={{ display: showForm ? '' : 'none' }} className={styles.formContainer}>
